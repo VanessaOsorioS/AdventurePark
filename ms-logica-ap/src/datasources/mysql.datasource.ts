8d5fc12db78aa4617de89c222890a41d84eb8334
seider sanin <seider.sanin22135@ucaldas.edu.co>
2022-10-06 09:22:31 -0500

    edicion datos datasource

--- ms-logica-ap/src/datasources/mysql.datasource.ts
+++ ms-logica-ap/src/datasources/mysql.datasource.ts
@@ -8,8 +8,15 @@ const config = {
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: 'Vane_1205',
-  database: 'adventurepark_db'
+  // Valeriana
+  password: '123456789',
+  database: 'adventurepark_db',
+  // Jhones
+  // password: 'unodos3tres',
+  // database: 'ap'
+  // Vanessa
+  // password: 'Vane_1205',
+  // database: 'adventurepark_db'
 };
 
 // Observe application's life cycle to disconnect the datasource when
@@ -17,8 +24,8 @@ const config = {
 // gracefully. The `stop()` method is inherited from `juggler.DataSource`.
 // Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
 @lifeCycleObserver('datasource')
-export class MysqlDataSource extends juggler.DataSource
-  implements LifeCycleObserver {
+  export class MysqlDataSource extends juggler.DataSource
+    implements LifeCycleObserver {
   static dataSourceName = 'mysql';
   static readonly defaultConfig = config;
 
